f1456d02ef16592030cad05b2851122327077e66
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-08 09:52:26 +0530

    Generated: `discord-bot-project/src/interfaces/musicAPI.js`

---
+++ discord-bot-project/src/interfaces/musicAPI.js
@@ -0,0 +1,42 @@
+const playMusic = (song) => {
+  // Logic to play music
+};
+
+const pauseMusic = () => {
+  // Logic to pause music
+};
+
+const skipMusic = () => {
+  // Logic to skip music
+};
+
+const addToQueue = (song) => {
+  // Logic to add song to queue
+};
+
+const adjustVolume = (volumeLevel) => {
+  // Logic to adjust volume
+};
+
+const enableLoop = () => {
+  // Logic to enable looping
+};
+
+const enableShuffle = () => {
+  // Logic to enable shuffling
+};
+
+const displayLyrics = (song) => {
+  // Logic to display lyrics
+};
+
+export {
+  playMusic,
+  pauseMusic,
+  skipMusic,
+  addToQueue,
+  adjustVolume,
+  enableLoop,
+  enableShuffle,
+  displayLyrics,
+};
