256fa82a35a75825e99282270f3dc6882d102c70
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-08 09:53:02 +0530

    Generated: `discord-bot-project/config/spotifyConfig.js`

---
+++ discord-bot-project/config/spotifyConfig.js
@@ -0,0 +1,7 @@
+const spotifyConfig = {
+    clientId: 'YOUR_SPOTIFY_CLIENT_ID',
+    clientSecret: 'YOUR_SPOTIFY_CLIENT_SECRET',
+    redirectUri: 'YOUR_REDIRECT_URI'
+};
+
+module.exports = spotifyConfig;
