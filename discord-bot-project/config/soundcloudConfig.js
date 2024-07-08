be6ab50937ae1aab2fbb6c4a466347868a8e6d45
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-08 09:53:05 +0530

    Generated: `discord-bot-project/config/soundcloudConfig.js`

---
+++ discord-bot-project/config/soundcloudConfig.js
@@ -0,0 +1,7 @@
+const soundcloudConfig = {
+  apiKey: "YOUR_SOUNDCLOUD_API_KEY_HERE",
+  apiSecret: "YOUR_SOUNDCLOUD_API_SECRET_HERE",
+  callbackURL: "http://localhost:3000/auth/soundcloud/callback"
+};
+
+module.exports = soundcloudConfig;
