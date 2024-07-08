bfe5bc1601070c70c4b9053e3836cc9f3dd358f6
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-08 09:52:59 +0530

    Generated: `discord-bot-project/config/discordConfig.js`

---
+++ discord-bot-project/config/discordConfig.js
@@ -0,0 +1,12 @@
+const discordConfig = {
+  token: 'YOUR_DISCORD_BOT_TOKEN',
+  prefix: '!',
+  defaultVolume: 50,
+  musicQueue: [],
+  isPlaying: false,
+  dispatcher: null,
+  voiceChannel: null,
+  textChannel: null
+};
+
+module.exports = discordConfig;
